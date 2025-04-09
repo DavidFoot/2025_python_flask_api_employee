8632ee3c2b472c0860df72fe56e9604e65425714
David F <david.foot@outlook.be>
2025-04-09 16:31:48 +0200

    Add Sqlite connexion to all endpoints

--- FlaskApi/app.py
+++ FlaskApi/app.py
@@ -16,32 +16,29 @@ employee_table  = Table(
     )
 metadata_obj.create_all(engine)
 
-employees = [ { 'id': 1, 'name': 'Ashley' }, { 'id': 2, 'name': 'Kate' }, { 'id': 3, 'name': 'Joe' }]
-
 @app.route('/employees', methods=['GET'])
 def get_employees():
     with engine.connect() as conn:
         result = conn.execute(text("SELECT * FROM employee"))
         employees_output = [dict(row._mapping) for row in result]
         return jsonify(employees_output), 200
-
 @app.route('/test_table_employee', methods=['GET'])
 def test_create_employee():
     with engine.connect() as conn:
         try:
             result = conn.execute(text("SELECT * FROM employee LIMIT 1"))
-            return jsonify({'message': 'Table employee exists and is accessible.', 'result': [row for row in result]}), 200
+            return jsonify({'message': 'Table employee exists and is accessible.'}), 200
         except Exception as e:
             return jsonify({'error': str(e)}), 500
-
 @app.route('/employees/<int:idx>', methods=['GET'])
 def get_employee(idx:int):
-    global employees
-    if len([e for e in employees if e["id"] == idx]) == 0 :
-        return jsonify({'error': 'Employee does not exist.'}), 404
+    if check_employee_by_id(idx):
+        with engine.connect() as conn:
+            result = conn.execute(text(f"SELECT * FROM employee WHERE id = {idx}"))
+            res = [dict(row._mapping) for row in result]
+            return jsonify({'success': res}), 200
     else:
-        return jsonify([e for e in employees if e["id"] == idx]) , 200
-
+        return jsonify({'error': 'Employee does not exist.'}), 404
 @app.route('/employees/<int:idx>', methods=['DELETE'])
 def del_employees(idx : int):
     if check_employee_by_id(idx) :
@@ -51,7 +48,6 @@ def del_employees(idx : int):
             return jsonify({'success': 'Employee deleted.'}), 200
     else:
         return jsonify({'error': 'Employee does not exist.'}), 404
-
 @app.route('/employees', methods=['POST'])
 def create_employee():
     new_employee = json.loads(request.data)
@@ -60,25 +56,19 @@ def create_employee():
         result = sqla_connection.execute(stmt)
         sqla_connection.commit()
         return jsonify({'id': result.inserted_primary_key[0], 'name': new_employee["name"], 'age': new_employee["age"]}), 201
-
 @app.route('/employees/<int:idx>', methods=['PUT'])
 def update_employee(idx : int):
-    global employees
-    if len([e for e in employees if e["id"] == idx]) == 0:
-        return jsonify({'error': 'Employee does not exist.'}), 404
+    if check_employee_by_id(idx):
+        update_data_employee = json.loads(request.data)
+        with engine.connect() as conn:
+            query = employee_table.update().values(name=update_data_employee["name"], age=update_data_employee["age"]).where(employee_table.c.id == f"{idx}")
+            conn.execute(query)
+            conn.commit()
+            return jsonify({'success': 'Employee Updated.'}), 200
     else:
-        new_employee_data = json.loads(request.data)
-        for key in new_employee_data.keys():
-            if key == "name":
-                employee = get_employee_by_id(idx)[0]
-                employee.update(new_employee_data)
-                return jsonify({'success': employee}), 200
+        return jsonify({'error': 'Employee does not exist.'}), 404
 
-def get_max_id():
-    global employees
-    max_id = max(e['id'] for e in employees)
-    return max_id
-
+#Utils
 def check_employee_by_id(idx: int):
     with engine.connect() as conn:
         result = conn.execute(text(f"SELECT * FROM employee where id = {idx}"))
@@ -86,10 +76,15 @@ def check_employee_by_id(idx: int):
             return True
         return False
 
+
+# no more Needed
+def get_max_id():
+    global employees
+    max_id = max(e['id'] for e in employees)
+    return max_id
 def get_employee_by_id(idx: int):
     global employees
     return [e for e in employees if e["id"] == idx]
-
 
 if __name__ == '__main__':
    app.run(debug=True, host='0.0.0.0',port=5000)
